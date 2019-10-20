6f76d602caf4cc9d4305fd4e72e6a766dcad6618
Niklas Schnettler <niklas.schnettler@gmail.com>
2019-10-21 01:50:44 +0200

    Navigation - Use BottomNavigation

--- app/src/main/java/de/schnettler/tvtracker/MainActivity.kt
+++ app/src/main/java/de/schnettler/tvtracker/MainActivity.kt
@@ -2,13 +2,25 @@ package de.schnettler.tvtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import de.schnettler.tvtracker.ui.main.MainFragment
+import androidx.databinding.DataBindingUtil
+import de.schnettler.tvtracker.databinding.MainActivityBinding
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
 
+        binding.root.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        NavigationUI.setupWithNavController(binding.bottomNavigation, this.findNavController(R.id.nav_host_fragment))
     }
 }
