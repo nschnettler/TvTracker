cf561d634ecc2f78300b6160c76a9b2dd1527499
Niklas Schnettler <niklas.schnettler@gmail.com>
2019-10-20 21:53:35 +0200

    MainFragment - DataBinding

--- app/src/main/java/de/schnettler/tvtracker/ui/main/MainFragment.kt
+++ app/src/main/java/de/schnettler/tvtracker/ui/main/MainFragment.kt
@@ -1,30 +1,22 @@
 package de.schnettler.tvtracker.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import de.schnettler.tvtracker.R
+import de.schnettler.tvtracker.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
 
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        val binding = MainFragmentBinding.inflate(inflater)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        return binding.root
     }
-
 }
