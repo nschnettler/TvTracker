dcc911bac4f8f71821f0d490fb90023b1f517cbe
Niklas Schnettler <niklas.schnettler@gmail.com>
2019-10-24 01:30:37 +0200

    Custom SwipeRefreshLayout (based on https://stackoverflow.com/questions/23989910/horizontalscrollview-inside-swiperefreshlayout)

--- app/src/main/java/de/schnettler/tvtracker/util/Utils.kt
+++ app/src/main/java/de/schnettler/tvtracker/util/Utils.kt
@@ -1,7 +1,13 @@
 package de.schnettler.tvtracker.util
 
 import android.content.Context
+import android.util.AttributeSet
 import android.widget.Toast
+import android.view.MotionEvent
+import android.view.ViewConfiguration
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import kotlin.math.abs
+
 
 const val TMDB_API_KEY = "***TMDB_API_KEY***"
 const val TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w185"
@@ -12,3 +18,30 @@ enum class StatusCodes(val code: Int) {
 }
 
 fun Context.makeToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+class SwipeRefreshLayout(context: Context, attrs: AttributeSet? = null) : SwipeRefreshLayout(context, attrs) {
+
+    private val touchSlop: Int = ViewConfiguration.get(context).scaledTouchSlop
+    private var previousX = 0F
+    private var declined: Boolean = false
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                previousX = event.x
+                declined = false
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                val eventX = event.x
+                val xDiff = abs(eventX - previousX)
+
+                if (declined || xDiff > touchSlop) {
+                    declined = true
+                    return false
+                }
+            }
+        }
+        return super.onInterceptTouchEvent(event)
+    }
+}
