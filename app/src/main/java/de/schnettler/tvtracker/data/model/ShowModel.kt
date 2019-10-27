528bfc946b532950b8d20c168730e1ba2c2a6254
Niklas Schnettler <niklas.schnettler@gmail.com>
2019-10-27 12:20:53 +0100

    ShowModel - Make Parcelable

--- app/src/main/java/de/schnettler/tvtracker/data/model/ShowModel.kt
+++ app/src/main/java/de/schnettler/tvtracker/data/model/ShowModel.kt
@@ -1,7 +1,11 @@
 package de.schnettler.tvtracker.data.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Show(
     val id: Long,
     val title: String,
     var posterUrl: String = ""
-)
+): Parcelable
