cbe5844108bd38ba06a06ab52f8eb1623d960c81
Niklas Schnettler <niklas.schnettler@gmail.com>
2019-11-22 18:47:05 +0100

    Show Model (Remote) - Slug can be null

--- app/src/main/java/de/schnettler/tvtracker/data/show/model/ShowRemote.kt
+++ app/src/main/java/de/schnettler/tvtracker/data/show/model/ShowRemote.kt
@@ -21,7 +21,7 @@ data class ShowRemote(
 
 data class ShowIdRemote(
     val trakt: Long,
-    val slug: String,
+    val slug: String?,
     val tvdb: Long?,
     val imdb: String?,
     val tmdb: Long?
