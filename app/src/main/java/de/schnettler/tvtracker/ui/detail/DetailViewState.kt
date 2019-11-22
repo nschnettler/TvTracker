cadaf96516139f4b5bc1612bb58b0f96e9370f7e
Niklas Schnettler <niklas.schnettler@gmail.com>
2019-11-22 12:59:08 +0100

    DetailViewState - Add seasons

--- app/src/main/java/de/schnettler/tvtracker/ui/detail/DetailViewState.kt
+++ app/src/main/java/de/schnettler/tvtracker/ui/detail/DetailViewState.kt
@@ -5,11 +5,13 @@ import de.schnettler.tvtracker.data.auth.model.AuthTokenDB
 import de.schnettler.tvtracker.data.show.model.Show
 import de.schnettler.tvtracker.data.show.model.ShowDetails
 import de.schnettler.tvtracker.data.show.model.cast.CastEntry
+import de.schnettler.tvtracker.data.show.model.season.SeasonDomain
 
 data class DetailViewState(
     val show: Show,
     val details: ShowDetails? = null,
     val tvdbAuth: AuthTokenDB? = null,
     val cast: List<CastEntry>? = null,
-    val relatedShows: List<Show>? = null
+    val relatedShows: List<Show>? = null,
+    val seasons: List<SeasonDomain>? = null
 ): ViewState
