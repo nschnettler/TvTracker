9230e0848a0101386f10650950fc2e29afc95118
Niklas Schnettler <niklas.schnettler@gmail.com>
2019-11-24 12:41:42 +0100

    Add Back OkHttp Dependency

--- app/src/main/java/de/schnettler/tvtracker/data/api/RetrofitClient.kt
+++ app/src/main/java/de/schnettler/tvtracker/data/api/RetrofitClient.kt
@@ -4,7 +4,6 @@ import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
