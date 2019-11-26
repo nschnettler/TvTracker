0829cef1162a2ca5cd5d46189bbb92ade6d931c3
Niklas Schnettler (Sh4dowSoul) <niklas.schnettler@gmail.com>
2019-11-27 00:39:29 +0100

    Discover UI - Display Recommended Shows when logged in

--- app/src/main/java/de/schnettler/tvtracker/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/de/schnettler/tvtracker/ui/discover/DiscoverFragment.kt
@@ -10,22 +10,25 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
+import de.schnettler.tvtracker.AuthViewModel
 import de.schnettler.tvtracker.data.models.ShowDomain
 import de.schnettler.tvtracker.databinding.DiscoverFragmentBinding
 import de.schnettler.tvtracker.util.ViewModelFactory
+import de.schnettler.tvtracker.util.getViewModel
 import dev.chrisbanes.insetter.doOnApplyWindowInsets
 
 class DiscoverFragment : Fragment() {
 
     private lateinit var viewModel: DiscoverViewModel
+    private lateinit var authViewModel: AuthViewModel
     private lateinit var controller: DiscoverController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val binding = DiscoverFragmentBinding.inflate(inflater)
         val application = requireNotNull(this.activity).application
-        viewModel = ViewModelProviders.of(this, ViewModelFactory(application))
-            .get(DiscoverViewModel::class.java)
+        viewModel = getViewModel {DiscoverViewModel(activity!!.application)}
+        authViewModel = getViewModel { AuthViewModel(activity!!.application) }
         binding.lifecycleOwner = this
         binding.viewmodel = viewModel
 
@@ -46,6 +49,17 @@ class DiscoverFragment : Fragment() {
         viewModel.anticipatedShows.observe(viewLifecycleOwner, Observer {
             controller.anticipatedShows = it
             controller.requestModelBuild()
+        })
+        viewModel.recommendedShows.observe(viewLifecycleOwner, Observer {
+            controller.recommendedShows = it
+            controller.requestModelBuild()
+        })
+
+        authViewModel.traktLoginStatus.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                viewModel.onLoginChanged(it, authViewModel.traktAuthToken.value!!.token)
+            }
+
         })
 
         //WindowInsets
--- app/src/main/java/de/schnettler/tvtracker/ui/discover/DiscoverViewModel.kt
+++ app/src/main/java/de/schnettler/tvtracker/ui/discover/DiscoverViewModel.kt
@@ -4,10 +4,13 @@ import android.app.Application
 import androidx.lifecycle.*
 import de.schnettler.tvtracker.data.api.RetrofitClient
 import de.schnettler.tvtracker.data.db.getDatabase
+import de.schnettler.tvtracker.data.models.EpisodeDomain
 import de.schnettler.tvtracker.data.repository.show.ShowDataSourceRemote
 import de.schnettler.tvtracker.data.repository.show.ShowRepository
 import de.schnettler.tvtracker.util.TopListType
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class DiscoverViewModel(val context: Application) : AndroidViewModel(context) {
@@ -20,10 +23,13 @@ class DiscoverViewModel(val context: Application) : AndroidViewModel(context) {
     val trendingShows = showRepository.getTopList(TopListType.TRENDING)
     val popularShows = showRepository.getTopList(TopListType.POPULAR)
     val anticipatedShows = showRepository.getTopList(TopListType.ANTICIPATED)
+    val recommendedShows = showRepository.getTopList(TopListType.RECOMMENDED)
+
+    private val _loggedIn = MutableLiveData<Boolean>()
+    val loggedIn: LiveData<Boolean> get() = _loggedIn
 
     private val _isRefreshing = MutableLiveData<Boolean>()
-    val isRefreshing: LiveData<Boolean>
-        get() = _isRefreshing
+    val isRefreshing: LiveData<Boolean> get() = _isRefreshing
 
     init {
         onRefresh()
@@ -37,5 +43,17 @@ class DiscoverViewModel(val context: Application) : AndroidViewModel(context) {
              showRepository.refreshShowList(TopListType.ANTICIPATED)
          }
          _isRefreshing.value = false
+    }
+
+    fun onLoginChanged(newValue: Boolean, token: String) {
+        _loggedIn.value = newValue
+
+        if (newValue) {
+            viewModelScope.launch {
+                withContext(Dispatchers.IO) {
+                    showRepository.refreshShowList(TopListType.RECOMMENDED, token)
+                }
+            }
+        }
     }
 }
